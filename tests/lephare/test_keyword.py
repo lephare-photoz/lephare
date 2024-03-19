4055623063b6e08e86c78f82410c3bb1cf074b0f
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2024-03-19 09:01:07 -0400

    Convert tests to pytest

--- tests/lephare/test_keyword.py
+++ tests/lephare/test_keyword.py
@@ -1,97 +1,101 @@
+import numpy as np
 import os
-import unittest
+import pytest
+import tempfile
 
 from lephare import keyword, read_command, read_config
 
 TESTDIR = os.path.abspath(os.path.dirname(__file__))
-TESTDATADIR = os.path.join(TESTDIR, "../data")
 LEPHAREDIR = os.path.join(TESTDIR, "..")
 
 
-class filter_tests(unittest.TestCase):
-    def test_basic(self):
-        # test that default is taken when value is empty
-        k = keyword("k1", "")
-        self.assertEqual(k.split_string("DEFAULT", 1)[0], "DEFAULT")
-        # check basic assignment
-        k = keyword("k2", "1")
-        self.assertEqual(k.name, "k2")
-        self.assertEqual(k.value, "1")
-        # test mismatch in size
-        self.assertEqual(k.split_string("0", 2), ["1", "1"])
-        k = keyword("k3", "1,1")
-        self.assertEqual(k.split_string("0", 3), ["0", "0", "0"])
+def test_keyword_basic():
+    # test that default is taken when value is empty
+    k = keyword("k1", "")
+    assert k.split_string("DEFAULT", 1)[0] == "DEFAULT"
+    # check basic assignment
+    k = keyword("k2", "1")
+    assert k.name == "k2"
+    assert k.value == "1"
+    # test mismatch in size
+    assert np.array_equal(k.split_string("0", 2), ["1", "1"])
+    k = keyword("k3", "1,1")
+    assert np.array_equal(k.split_string("0", 3), ["0", "0", "0"])
 
-    def test_array(self):
-        k = keyword("k", "1,1")
-        self.assertEqual(k.split_string("0", 2), ["1", "1"])
-        self.assertEqual(k.split_int("0", 2), [1, 1])
-        self.assertEqual(k.split_long("0", 2), [1, 1])
-        self.assertEqual(k.split_bool("0", 2), [True, True])
-        self.assertEqual(k.split_double("0", 2), [1.0, 1.0])
 
-    def test_bool(self):
-        k = keyword("k", "1")
-        self.assertEqual(k.split_bool("0", 1)[0], True)
-        k = keyword("k", "YES")
-        self.assertEqual(k.split_bool("0", 1)[0], True)
-        k = keyword("k", "yes")
-        self.assertEqual(k.split_bool("0", 1)[0], True)
-        k = keyword("k", "True")
-        self.assertEqual(k.split_bool("0", 1)[0], True)
-        k = keyword("k", "true")
-        self.assertEqual(k.split_bool("0", 1)[0], True)
-        k = keyword("k", "ANYTHING ELSE")
-        self.assertEqual(k.split_bool("0", 1)[0], False)
+def test_keyword_array():
+    k = keyword("k", "1,1")
+    assert np.array_equal(k.split_string("0", 2), ["1", "1"])
+    assert np.array_equal(k.split_int("0", 2), [1, 1])
+    assert np.array_equal(k.split_long("0", 2), [1, 1])
+    assert np.array_equal(k.split_bool("0", 2), [True, True])
+    assert np.array_equal(k.split_double("0", 2), [1.0, 1.0])
 
-    def test_expand_path(self):
-        lepharedir = os.environ["LEPHAREDIR"]
-        home = os.environ["HOME"]
-        k = keyword("k", "$LEPHAREDIR/unittest")
-        self.assertEqual(k.value, os.path.join(lepharedir, "unittest"))
-        k = keyword("k", "$HOME/unittest")
-        self.assertEqual(k.value, os.path.join(home, "unittest"))
 
-    def test_read_config(self):
-        config_file = os.path.join(TESTDATADIR, "examples/COSMOS.para")
-        km = read_config(config_file)
-        for k in km.keys():
-            self.assertNotEqual(k[0], "#")
-            self.assertNotEqual(k[0], " ")
-            self.assertNotEqual(k[0], "\t")
+def test_keyword_bool():
+    k = keyword("k", "1")
+    assert k.split_bool("0", 1)[0]
+    k = keyword("k", "YES")
+    assert k.split_bool("0", 1)[0]
+    k = keyword("k", "yes")
+    assert k.split_bool("0", 1)[0]
+    k = keyword("k", "True")
+    assert k.split_bool("0", 1)[0]
+    k = keyword("k", "true")
+    assert k.split_bool("0", 1)[0]
+    k = keyword("k", "ANYTHING ELSE")
+    assert not k.split_bool("0", 1)[0]
 
-    def test_read_command(self):
-        km = read_command(["exec", "-c", "config", "--arg1", "val1", "-arg2", "val2"])
-        for k in km.keys():
-            if k == "c":
-                self.assertEqual(km[k].value, "config")
-            if k == "arg1":
-                self.assertEqual(km[k].value, "val1")
-            if k == "arg2":
-                self.assertEqual(km[k].value, "val2")
 
-    def test_misformed_kwd(self):
-        # normal
-        k = keyword("k", "1,2,3")
-        self.assertEqual(k.split_string("0", -1), ["1", "2", "3"])
-        # space
-        k = keyword("k", "1,2, 3")
-        self.assertEqual(k.split_string("0", -1), ["1", "2", "3"])
-        # tab
-        k = keyword("k", "1,2,\t3")
-        self.assertEqual(k.split_string("0", -1), ["1", "2", "3"])
-        with open("tmp", "w") as f:
+def test_keyword_expand_path():
+    lepharedir = os.environ["LEPHAREDIR"]
+    home = os.environ["HOME"]
+    k = keyword("k", "$LEPHAREDIR/unittest")
+    assert k.value == os.path.join(lepharedir, "unittest")
+    k = keyword("k", "$HOME/unittest")
+    assert k.value == os.path.join(home, "unittest")
+
+
+def test_keyword_read_config(test_data_dir):
+    config_file = os.path.join(test_data_dir, "examples/COSMOS.para")
+    km = read_config(config_file)
+    for k in km.keys():
+        assert k[0] != "#"
+        assert k[0] != " "
+        assert k[0] != "\t"
+
+
+def test_keyword_read_command():
+    km = read_command(["exec", "-c", "config", "--arg1", "val1", "-arg2", "val2"])
+    for k in km.keys():
+        if k == "c":
+            assert km[k].value == "config"
+        if k == "arg1":
+            assert km[k].value == "val1"
+        if k == "arg2":
+            assert km[k].value == "val2"
+
+
+def test_keyword_misformed_kwd():
+    # normal
+    k = keyword("k", "1,2,3")
+    assert np.array_equal(k.split_string("0", -1), ["1", "2", "3"])
+    # space
+    k = keyword("k", "1,2, 3")
+    assert np.array_equal(k.split_string("0", -1), ["1", "2", "3"])
+    # tab
+    k = keyword("k", "1,2,\t3")
+    assert np.array_equal(k.split_string("0", -1), ["1", "2", "3"])
+
+    with tempfile.TemporaryDirectory() as dir_name:
+        filename = os.path.join(dir_name, "tmp")
+        with open(filename, "w") as f:
             f.writelines(
                 [
                     "FILTER_CALIB    0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,\t0",
                     "\nFILTER_FILE     filter_cosmos",
                 ]
             )
-        km = read_config("tmp")
-        self.assertEqual(km["FILTER_CALIB"].value, "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
-        self.assertEqual(km["FILTER_FILE"].value, "filter_cosmos")
-        os.remove("tmp")
-
-
-if __name__ == "__main__":
-    unittest.main()
+        km = read_config(filename)
+        assert np.array_equal(km["FILTER_CALIB"].value, "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+        assert np.array_equal(km["FILTER_FILE"].value, "filter_cosmos")
--- tests/lephare/test_oneElLambda.py
+++ tests/lephare/test_oneElLambda.py
@@ -1,63 +1,44 @@
 import os
-import unittest
+import pytest
 from lephare import _lephare
 
-TESTDIR = os.path.abspath(os.path.dirname(__file__))
-TESTDATADIR = os.path.join(TESTDIR, "../data")
+
+def test_oneElLambda_constructor():
+    el1 = _lephare.oneElLambda(10, 10, 0)
+    # test that 1 + 1 = 2
+    assert el1.lamb == 10
+    assert el1.val == 10
+    assert el1.ori == 0
+    el2 = _lephare.oneElLambda(el1)
+    assert el1.lamb == el2.lamb
+    assert el1.val == el2.val
+    assert el1.ori == el2.ori
 
 
-class oneElLambda(unittest.TestCase):
-    def test_constructor(self):
-        el1 = _lephare.oneElLambda(10, 10, 0)
-        # test that 1 + 1 = 2
-        self.assertEqual(el1.lamb, 10)
-        self.assertEqual(el1.val, 10)
-        self.assertEqual(el1.ori, 0)
-        el2 = _lephare.oneElLambda(el1)
-        self.assertEqual(el1.lamb, el2.lamb)
-        self.assertEqual(el1.val, el2.val)
-        self.assertEqual(el1.ori, el2.ori)
-
-    def test_interp(self):
-        el1 = _lephare.oneElLambda(10, 10, 0)
-        el2 = _lephare.oneElLambda(20, 20, 0)
-        el3 = _lephare.oneElLambda(15, 0, 0)
-        el3.interp(el1, el2)
-        self.assertEqual(el3.val, 15)
-        # next fails as interp expect ordering
-        # which could be made to work
-        # el3.interp(el2,el1)
-        # self.assertEqual(el3.val, 15)
-        pass
+def test_oneElLambda_interp():
+    el1 = _lephare.oneElLambda(10, 10, 0)
+    el2 = _lephare.oneElLambda(20, 20, 0)
+    el3 = _lephare.oneElLambda(15, 0, 0)
+    el3.interp(el1, el2)
+    assert el3.val == 15
 
 
-class ext(unittest.TestCase):
-    def test_constructor(self):
-        ext = _lephare.ext(name="test", numext=10)
-        self.assertEqual(ext.name, "test")
-        self.assertEqual(ext.numext, 10)
-        self.assertEqual(len(ext.lamb_ext), 0)
-
-    def test_read(self):
-        filename = os.path.join(TESTDATADIR, "calzetti.dat")
-        ext = _lephare.ext(name="test", numext=10)
-        ext.read(filename)
-        self.assertEqual(ext.lmin(), 400.0)
-        self.assertEqual(ext.lmax(), 40000.0)
+def test_oneElLambda_constructor():
+    ext = _lephare.ext(name="test", numext=10)
+    assert ext.name == "test"
+    assert ext.numext == 10
+    assert len(ext.lamb_ext) == 0
 
 
-class ext(unittest.TestCase):
-    def test_constructor(self):
-        ext = _lephare.ext(name="test", numext=10)
-        self.assertEqual(ext.name, "test")
-        self.assertEqual(ext.numext, 10)
-        self.assertEqual(len(ext.lamb_ext), 0)
-
-    def test_read(self):
-        filename = os.path.join(TESTDATADIR, "tau10.out")
-        ext = _lephare.ext(name="test", numext=10)
-        ext.read(filename)
+def test_oneElLambda_read_calzetti(test_data_dir):
+    filename = os.path.join(test_data_dir, "calzetti.dat")
+    ext = _lephare.ext(name="test", numext=10)
+    ext.read(filename)
+    assert ext.lmin == 400.0
+    assert ext.lmax == 40000.0
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_oneElLambda_read_tau10(test_data_dir):
+    filename = os.path.join(test_data_dir, "tau10.out")
+    ext = _lephare.ext(name="test", numext=10)
+    ext.read(filename)
