ab9bbb120aa585addb31b3868d2d7ed2448516e3
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2024-03-19 12:05:22 -0400

    Try overriding "LEPHAREDIR" for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import os
-
 import pytest
 
+# Set "LEPHAREDIR" locally for tests if it is not already set.
+if "LEPHAREDIR" not in os.environ:
+    test_dir = os.path.abspath(os.path.dirname(__file__))
+    os.environ["LEPHAREDIR"] = os.path.join(test_dir, "..")
 
 @pytest.fixture
 def test_data_dir():
