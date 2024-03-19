ed8693b4e0e20b284a8e8633c08d75e74d067084
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2024-03-19 12:10:31 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,11 @@ if "LEPHAREDIR" not in os.environ:
     test_dir = os.path.abspath(os.path.dirname(__file__))
     os.environ["LEPHAREDIR"] = os.path.join(test_dir, "..")
 
+# Set "LEPHAREWORK" locally for tests if it is not already set.
+if "LEPHAREWORK" not in os.environ:
+    test_dir = os.path.abspath(os.path.dirname(__file__))
+    os.environ["LEPHAREWORK"] = os.path.join(test_dir, "tmp")
+
 @pytest.fixture
 def test_data_dir():
     test_dir = os.path.abspath(os.path.dirname(__file__))
