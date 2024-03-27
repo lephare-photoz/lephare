e9d1e7a0992d98477177cda4f00f931fded0db19
drewoldag <47493171+drewoldag@users.noreply.github.com>
2024-03-27 10:58:38 -0700

    Adding the hack Raphael used to get mac builds working locally.

--- setup.py
+++ setup.py
@@ -59,9 +59,9 @@ class CMakeBuild(build_ext):
             build_args += ["--", "-j2"]
 
         env = os.environ.copy()
-        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get("CXXFLAGS", ""), self.distribution.get_version()
-        )
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+       '-g -std=c++17 -fopenmp -Wall -fno-inline -Wno-psabi', #env.get('CXXFLAGS', ''),
+       self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
         subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
