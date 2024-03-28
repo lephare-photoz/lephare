a67e3e2a5a3a3622058752e33af8e9bad4ff82e6
drewoldag <47493171+drewoldag@users.noreply.github.com>
2024-03-28 13:00:20 -0700

    Trying `PYTHON_EXECUTABLE=${which python}`.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ class CMakeBuild(build_ext):
         cmake_args = [
             "-DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}",
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
-            # "-DPYTHON_EXECUTABLE=" + sys.executable,
+            "-DPYTHON_EXECUTABLE=${which python}",
         ]
 
         cfg = "Debug" if self.debug else "Release"
