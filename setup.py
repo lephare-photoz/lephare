8a8b224ded2f451e5769981815f65b7c539b6645
drewoldag <47493171+drewoldag@users.noreply.github.com>
2024-03-27 21:21:26 -0700

    Change method for finding python version to build for.

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ class CMakeBuild(build_ext):
         cmake_args = [
             "-DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}",
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
-            "-DPYTHON_EXECUTABLE=" + sys.executable,
+            # "-DPYTHON_EXECUTABLE=" + sys.executable,
+            "DPYBIND11_FINDPYTHON=ON",
         ]
 
         cfg = "Debug" if self.debug else "Release"
