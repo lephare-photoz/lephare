7b6afe0a546bef020d72959375788c463733e792
Raphael Shirley <rshirley@mpe.mpg.de>
2024-04-01 21:49:19 +0200

    First pass at setting default environment variables
    We get variables if present. If not present we set them to default values and use a symlink to a timestamped
    work directory which will allow overwriting by another process to create new runs. later addition of config
    writing to the directory to be done be distinct process

--- src/lephare/__init__.py
+++ src/lephare/__init__.py
@@ -1,25 +1,68 @@
 # ruff: noqa: E402
 # ruff: noqa: F403
 
+import datetime
+import logging
 import os
 
+# Why is this global?
 global LEPHAREDIR
+# Get the default cache location
+if os.name == "nt":
+    this_os_cache = os.getenv("LOCALAPPDATA")
+else:
+    this_os_cache = os.getenv("XDG_CACHE_HOME", os.path.expanduser("~/Library/Caches"))
 
-try:
-    LEPHAREDIR = os.environ["LEPHAREDIR"]
-except KeyError:
-    raise RuntimeError("Environment variable LEPHAREDIR has not been set")  # noqa: B904
+# Check if user defined environment variables present
+LEPHAREDIR = os.environ.get("LEPHAREDIR", None)
+# If not set set to default and make directories if not present
+if LEPHAREDIR is None:
+    # Default location in system cache
+    LEPHAREDIR = this_os_cache + "/lephare/data"
+    # Set environment variable to default
+    os.environ["LEPHAREDIR"] = LEPHAREDIR
+    if os.path.isdir(LEPHAREDIR):
+        pass
+    else:
+        logging.warning(
+            f"""Lephare default cache directory being created at
+            {LEPHAREDIR}. More than 1Gb may be written there."""
+        )
+        os.mkdir(LEPHAREDIR)
+else:
+    logging.warning(
+        f"""User defined LEPHAREDIR is set. Code runs depend on all required
+        auxilliary data being present at {LEPHAREDIR}."""
+    )
 
-try:
-    os.mkdir(os.environ["LEPHAREWORK"])
-    os.mkdir(os.path.join(os.environ["LEPHAREWORK"], "filt"))
-    os.mkdir(os.path.join(os.environ["LEPHAREWORK"], "lib_bin"))
-    os.mkdir(os.path.join(os.environ["LEPHAREWORK"], "lib_mag"))
-    os.mkdir(os.path.join(os.environ["LEPHAREWORK"], "zphota"))
-except FileExistsError:
-    pass
-except KeyError:
-    raise RuntimeError("Environment variable LEPHAREWORK has not been set")  # noqa: B904
+LEPHAREWORK = os.environ.get("LEPHAREWORK", None)
+# If not set thenset to default and make directories if not present
+if LEPHAREWORK is None:
+    # Default location in system cache
+    LEPHAREWORK = this_os_cache + "/lephare/work"
+    # Set environment variable to default
+    os.environ["LEPHAREWORK"] = LEPHAREWORK
+    if os.path.isdir(LEPHAREWORK):
+        pass
+    else:
+        # Make a timestamped 'run' directory which is symlinked from default
+        now = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+        RUNDIR = f"{LEPHAREWORK}/runs/{now}"
+        logging.warning(
+            f"""Lephare default working directory being created at
+            {LEPHAREWORK}."""
+        )
+        os.mkdir(RUNDIR)
+        os.symlink(RUNDIR, LEPHAREWORK)
+        os.mkdir(os.path.join(os.environ["LEPHAREWORK"], "filt"))
+        os.mkdir(os.path.join(os.environ["LEPHAREWORK"], "lib_bin"))
+        os.mkdir(os.path.join(os.environ["LEPHAREWORK"], "lib_mag"))
+        os.mkdir(os.path.join(os.environ["LEPHAREWORK"], "zphota"))
+else:
+    logging.warning(
+        f"""User defined LEPHAREWORK is set. All intermediate files will
+         be written to {LEPHAREWORK}."""
+    )
 
 
 from ._lephare import *
@@ -32,6 +75,7 @@ from ._flt import *
 from ._pdf import *
 from ._photoz import *
 from ._spec import *
+from ._version import *
 from .filter import *
 from .filterSvc import *
 from .mag_gal import *
