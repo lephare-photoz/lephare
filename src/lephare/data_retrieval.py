197e7069b133a2a6694c1b18ecf229a4f4150d98
Drew Oldag <awoldag@uw.edu>
2024-04-06 16:16:29 -0700

    Added a default constant in data_retrieval, updated docstring, changed the failing test to use the MAX_RETRY_ATTEMPTS const and added another test that sets the `retry` to something other than the default.

--- src/lephare/data_retrieval.py
+++ src/lephare/data_retrieval.py
@@ -23,6 +23,8 @@ DEFAULT_REGISTRY_FILE = "data_registry.txt"
 #  default cache location and return its path
 DEFAULT_LOCAL_DATA_PATH = "./data"
 
+# If a file is not downloaded the first time, retry this many times
+MAX_RETRY_ATTEMPTS = 2
 
 __all__ = [
     "download_all_files",
@@ -229,7 +231,7 @@ def download_file(retriever, file_name, ignore_registry=False):
         return retriever.fetch(file_name)
 
 
-def download_all_files(retriever, file_names, ignore_registry=False, retry=2):
+def download_all_files(retriever, file_names, ignore_registry=False, retry=MAX_RETRY_ATTEMPTS):
     """Download all files in the given list using the retriever.
 
     Parameters
@@ -240,6 +242,8 @@ def download_all_files(retriever, file_names, ignore_registry=False, retry=2):
         List of file names to download.
     ignore_registry : bool
         If True, download the files without checking their hashes against the registry.
+    retry : int
+        Number of times to retry downloading a file if first attempt fails.
 
     Returns
     -------
