c354fe659229be53e6cd8abadd4c5c8ec222120a
Olivia R. Lynn <olynn@andrew.cmu.edu>
2024-04-24 14:08:26 -0700

    Change patches from dectorators to context managers, which imo does improve readability

--- tests/lephare/test_data_retrieval_registry.py
+++ tests/lephare/test_data_retrieval_registry.py
@@ -20,7 +20,7 @@ Call download_registry_from_github
 
 import os
 import tempfile
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 import pytest
 import requests
@@ -29,100 +29,153 @@ from lephare.data_retrieval import (
 )
 
 
-@patch("requests.get")
-def test_download_registry_success(mock_get):
-    # 1. Local registry does not exist
+def test_download_registry_success():
+    # 1. Local registry does not exist (so no mocking needed here)
     #     1. Successfully downloaded registry file
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.text = "file1\nfile2\nfile3"
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        download_registry_from_github(outfile=os.path.join(tmpdir, "registry.txt"))
+    # Mock remote registry file
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.text = "file1\nfile2\nfile3"
+    with patch("requests.get", return_value=mock_response) as mock_get_remote_registry:  # noqa: F841
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            registry_outfile = os.path.join(tmp_dir, "registry.txt")
+            download_registry_from_github(outfile=registry_outfile)
+            # Check that we can open it (and it contains expected content)
+            with open(registry_outfile, "r") as file:
+                assert file.read() == "file1\nfile2\nfile3"
 
-        with open(os.path.join(tmpdir, "registry.txt"), "r") as file:
-            assert file.read() == "file1\nfile2\nfile3"
 
-
-@patch("requests.get")
-def test_download_registry_failure(mock_get):
-    # 1. Local registry does not exist
+def test_download_registry_failure():
+    # 1. Local registry does not exist (no mocking needed)
     #     2. Fail to download registry file
-    mock_get.return_value.status_code = 404
-    with pytest.raises(requests.exceptions.HTTPError):
-        download_registry_from_github()
+
+    # Mock failed registry file download
+    mock_response = Mock()
+    mock_response.status_code = 404
+    with patch("requests.get", return_value=mock_response) as mock_get_remote_registry:  # noqa: F841
+        with pytest.raises(requests.exceptions.HTTPError):
+            download_registry_from_github()
 
 
-@patch("os.path.isfile")
-@patch("pooch.file_hash")
-@patch("requests.get")
-def test_update_registry_hash_matches(mock_get, mock_file_hash, mock_isfile):
+def test_update_registry_hash_matches():
     # 2. Local registry exists
     #     1. _check_registry_is_latest_version == True
-    mock_isfile.return_value = True
-    mock_file_hash.return_value = "registryhash123"
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.text = "registryhash123"
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        outfile = os.path.join(tmpdir, "registry.txt")
-        download_registry_from_github(url="http://example.com/data_registry.txt", outfile=outfile)
+    # Mock the local registry file existing
+    with patch("os.path.isfile", return_value=True) as mock_local_registry_existing:  # noqa: F841
+        # Mock local registry having a certain pooch hash
+        with patch("pooch.file_hash", return_value="registryhash123") as mock_local_registry_hash:  # noqa: F841
+            # Mock getting the remote registry hash file
+            mock_response = Mock()
+            mock_response.status_code = 200
+            mock_response.text = "registryhash123"
+            with patch("requests.get", return_value=mock_response) as mock_get_remote_hash_file:
+                # Call the function
+                with tempfile.TemporaryDirectory() as tmp_dir:
+                    registry_outfile = os.path.join(tmp_dir, "registry.txt")
+                    download_registry_from_github(
+                        url="http://example.com/data_registry.txt", outfile=registry_outfile
+                    )
+                    # Assert that we only make 1 request for the hash file, and that we used the right url:
+                    assert mock_get_remote_hash_file.call_count == 1
+                    assert (
+                        mock_get_remote_hash_file.call_args[0][0]
+                        == "http://example.com/data_registry_hash.sha256"
+                    )
 
-        assert mock_get.call_count == 1
-        assert mock_get.call_args[0][0] == "http://example.com/data_registry_hash.sha256"
 
-
-@patch("os.path.isfile")
-@patch("pooch.file_hash")
-@patch("requests.get")
-def test_update_registry_hash_mismatches(mock_get, mock_file_hash, mock_isfile):
+def test_update_registry_hash_mismatches():
     # 2. Local registry exists
     #     2. _check_registry_is_latest_version == False
     #         1. Successfully downloaded registry file
-    mock_isfile.return_value = True
-    mock_file_hash.return_value = "registryhash123"
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.text = "hash_doesn't_match123"
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        outfile = os.path.join(tmpdir, "registry.txt")
-        download_registry_from_github(url="http://example.com/data_registry.txt", outfile=outfile)
+    # Mock the local registry file existing
+    with patch("os.path.isfile", return_value=True) as mock_local_registry_existing:  # noqa: F841
+        # Mock local registry having a certain pooch hash
+        with patch("pooch.file_hash", return_value="registryhash123") as mock_local_registry_hash:  # noqa: F841
+            # Mock getting the remote hash/registry files
+            mock_hash_response = Mock()
+            mock_hash_response.status_code = 200
+            mock_hash_response.text = "hash_doesn't_match123"
 
-        # One call to download the hash file, one call to download the full registry file:
-        assert mock_get.call_count == 2
-        # The following set of [0][0][0] and such is because the call args list is
-        #     [call('http://example.com/data_registry_hash.sha256', timeout=60),
-        #      call('http://example.com/data_registry.txt', timeout=60)]
-        # and we're only interested in checking the urls:
-        assert mock_get.call_args_list[0][0][0] == "http://example.com/data_registry_hash.sha256"
-        assert mock_get.call_args_list[1][0][0] == "http://example.com/data_registry.txt"
+            mock_registry_response = Mock()
+            mock_registry_response.status_code = 200
+            mock_registry_response.text = "file1\nfile2\nfile3"
+
+            def which_mock_get(*args, **kwargs):
+                url = args[0]
+                if "hash.sha256" in url:
+                    return mock_hash_response
+                else:
+                    return mock_registry_response
+
+            with patch("requests.get", side_effect=which_mock_get) as mock_remote_files_get:
+                # Call the function
+                with tempfile.TemporaryDirectory() as tmp_dir:
+                    registry_outfile = os.path.join(tmp_dir, "registry.txt")
+                    download_registry_from_github(
+                        url="http://example.com/data_registry.txt", outfile=registry_outfile
+                    )
+                    # Checks:
+                    # One call to download the hash file, one call to download the full registry file:
+                    assert mock_remote_files_get.call_count == 2
+                    # The following set of [0][0][0] and such is because the call args list is
+                    #     [call('http://example.com/data_registry_hash.sha256', timeout=60),
+                    #      call('http://example.com/data_registry.txt', timeout=60)]
+                    # and we're only interested in checking the urls:
+                    assert (
+                        mock_remote_files_get.call_args_list[0][0][0]
+                        == "http://example.com/data_registry_hash.sha256"
+                    )
+                    assert (
+                        mock_remote_files_get.call_args_list[1][0][0]
+                        == "http://example.com/data_registry.txt"
+                    )
 
 
-@patch("os.path.isfile")
-@patch("pooch.file_hash")
-@patch("requests.get")
-def test_update_registry_hash_mismatches_and_download_fails(mock_get, mock_file_hash, mock_isfile):
+def test_update_registry_hash_mismatches_and_download_fails():
     # 2. Local registry exists
     #     2. _check_registry_is_latest_version == False
     #         2. Fail to download registry file
-    mock_isfile.return_value = True
-    mock_file_hash.return_value = "registryhash123"
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.text = "hash_doesn't_match123"
 
-    mock_get.return_value.status_code = 404
-    with pytest.raises(requests.exceptions.HTTPError):
-        download_registry_from_github()
+    # Mock the local registry file existing
+    with patch("os.path.isfile", return_value=True) as mock_local_registry_existing:  # noqa: F841
+        # Mock local registry having a certain pooch hash
+        with patch("pooch.file_hash", return_value="registryhash123") as mock_local_registry_hash:  # noqa: F841
+            # Mock getting the remote hash/registry files
+            mock_hash_response = Mock()
+            mock_hash_response.status_code = 200
+            mock_hash_response.text = "hash_doesn't_match123"
+
+            mock_registry_response = Mock()
+            mock_registry_response.status_code = 404
+
+            def which_mock_get(*args, **kwargs):
+                url = args[0]
+                if "hash.sha256" in url:
+                    return mock_hash_response
+                else:
+                    return mock_registry_response
+
+            with patch("requests.get", side_effect=which_mock_get) as mock_remote_files_get:  # noqa: F841
+                # Check that we raise HTTPError as expected
+                with pytest.raises(requests.exceptions.HTTPError):
+                    download_registry_from_github()
 
 
-@patch("os.path.isfile")
-@patch("pooch.file_hash")
-@patch("requests.get")
-def test_update_registry_hash_download_fails(mock_get, mock_file_hash, mock_isfile):
+def test_update_registry_hash_download_fails():
     # 2. Local registry exists
     #     3. Fail to download registry hash file
-    mock_isfile.return_value = True
-    mock_file_hash.return_value = "registryhash123"
 
-    mock_get.return_value.status_code = 404
-    with pytest.raises(requests.exceptions.RequestException):
-        download_registry_from_github()
+    # Mock the local registry file existing
+    with patch("os.path.isfile", return_value=True) as mock_local_registry_existing:  # noqa: F841
+        # Mock local registry having a certain pooch hash
+        with patch("pooch.file_hash", return_value="registryhash123") as mock_local_registry_hash:  # noqa: F841
+            # Mock getting the remote hash/registry files
+            mock_hash_response = Mock()
+            mock_hash_response.status_code = 404
+            with patch("requests.get", return_value=mock_hash_response) as mock_get_remote_hash_file:  # noqa: F841
+                # Check that we get the expected exception
+                with pytest.raises(requests.exceptions.RequestException):
+                    download_registry_from_github()
