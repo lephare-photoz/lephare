28581692c2e13f949c2370a93c60a08005b76d53
Raphael Shirley <rshirley@mpe.mpg.de>
2024-04-29 16:18:41 +0200

    Got tests passing locally by setting envirnoment variables explicity in test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,15 @@ import os
 
 import pytest
 
-# Always use test area "LEPHAREDIR".
+# Set "LEPHAREDIR" locally for tests if it is not already set.
+if "LEPHAREDIR" not in os.environ:
+    test_dir = os.path.abspath(os.path.dirname(__file__))
+    os.environ["LEPHAREDIR"] = os.path.join(test_dir, "data")
 
-test_dir = os.path.abspath(os.path.dirname(__file__))
-os.environ["LEPHAREDIR"] = os.path.join(test_dir, "data")
-
-# Always use test area "LEPHAREWORK".
-test_dir = os.path.abspath(os.path.dirname(__file__))
-os.environ["LEPHAREWORK"] = os.path.join(test_dir, "tmp")
+# Set "LEPHAREWORK" locally for tests if it is not already set.
+if "LEPHAREWORK" not in os.environ:
+    test_dir = os.path.abspath(os.path.dirname(__file__))
+    os.environ["LEPHAREWORK"] = os.path.join(test_dir, "tmp")
 
 
 @pytest.fixture
--- tests/lephare/test_prepare.py
+++ tests/lephare/test_prepare.py
@@ -4,7 +4,8 @@ import lephare as lp
 
 
 def test_prepare(test_data_dir):
-    print(os.environ["LEPHAREDIR"])
+    test_dir = os.path.abspath(os.path.dirname(__file__))
+    os.environ["LEPHAREDIR"] = os.path.join(test_dir, "../data")
+    os.environ["LEPHAREWORK"] = os.path.join(test_dir, "../tmp")
     config = lp.read_config(os.path.join(test_data_dir, "examples/COSMOS.para"))
-
     lp.prepare(config)
