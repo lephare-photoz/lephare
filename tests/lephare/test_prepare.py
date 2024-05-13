b55bc1dce10bb62112b71a1a464c340ad72e14e1
Raphael Shirley <rshirley@mpe.mpg.de>
2024-05-14 01:59:12 +0200

    Added further check in prepare test

--- tests/lephare/test_prepare.py
+++ tests/lephare/test_prepare.py
@@ -10,14 +10,18 @@ def test_prepare(test_data_dir):
     os.environ["LEPHAREWORK"] = os.path.join(test_dir, "../tmp")
     config = lp.read_config(os.path.join(test_data_dir, "examples/COSMOS.para"))
     lp.prepare(config)
+    # Check it made the galaxy binary file
+    assert os.path.exists(os.path.join(test_dir, "../tmp/lib_mag/CE_COSMOS.bin"))
 
 
 def test_load_sed_list(test_data_dir):
     test_dir = os.path.abspath(os.path.dirname(__file__))
     # Move one of the example sed folders
-    _ = shutil.copytree(os.path.join(test_dir, "../data/sed/QSO"), os.path.join(test_dir, "../tmp/tmp"))
-    lp.load_sed_list(os.path.join(test_dir, "../tmp/tmp/ONE_SED.list"), "QSO")
+    _ = shutil.copytree(os.path.join(test_dir, "../data/sed/QSO"), os.path.join(test_dir, "../tmp/seds"))
+    lp.load_sed_list(os.path.join(test_dir, "../tmp/seds/ONE_SED.list"), "QSO")
     # Check the list is there
     assert os.path.exists(os.path.join(test_dir, "../data/sed/QSO/ONE_SED/ONE_SED.list"))
     # Check the sed is there
     assert os.path.exists(os.path.join(test_dir, "../data/sed/QSO/ONE_SED/o5v.sed.ext"))
+    # Clear the copied folder
+    shutil.rmtree(os.path.join(test_dir, "../tmp/seds"))
