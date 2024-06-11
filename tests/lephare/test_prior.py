864841240c1bb7cf4978614d04f0a09d4a7cb24f
Raphael Shirley <rshirley@mpe.mpg.de>
2024-06-11 16:33:42 +0200

    Added base test for prior functionality
    What else should we test and how do we get high coverage of the c side
    
    Need to test the nz prior at least

---
+++ tests/lephare/test_prior.py
@@ -0,0 +1,42 @@
+import os
+
+import lephare as lp
+import numpy as np
+from astropy.table import Table
+
+
+def test_prior(test_data_dir: str):
+    test_dir = os.path.abspath(os.path.dirname(__file__))
+    os.environ["LEPHAREDIR"] = os.path.join(test_dir, "../data")
+    os.environ["LEPHAREWORK"] = os.path.join(test_dir, "../tmp")
+    # Read the config file.
+    config_file = os.path.join(test_data_dir, "examples/COSMOS.para")
+    config = lp.read_config(config_file)
+    # Run preparation tasks.
+    lp.prepare(config)
+    # Read the test input catalogue
+    input_file = os.path.join(test_data_dir, "examples/COSMOS_first100specz.fits")
+    input = Table.read(input_file)
+    # Make a reduced column set for the minimal test
+    reduced_cols = []
+    for c in input.colnames:
+        if not c.startswith("f"):
+            reduced_cols.append(c)
+        elif "IB527" in c:
+            reduced_cols.append(c)
+        elif "IB679" in c:
+            reduced_cols.append(c)
+
+    # initialise the prior
+    def example_prior(lib, obj):
+        """An example prior which prohibits redshift above 2"""
+        weight = 1.0  # Be default do not change prior
+        if lib.red > 2:
+            weight = 1.0e-9
+        return weight
+
+    output, pdfs, zgrid = lp.process(config, input[reduced_cols], prior=example_prior)
+    # Check one of the outputs (results are terrible with just one filter and sparse z grid)
+    assert ~np.isclose(output["Z_BEST"][0], 3.58577857627795)  # Should not equal the original redshift
+    assert len(zgrid) == 51
+    assert np.sum(output["Z_BEST"] > 2) == 0
