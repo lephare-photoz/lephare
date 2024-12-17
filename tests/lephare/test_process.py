fe79342d863f8374fc34f9fda52eb625bbb9ac7e
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2024-12-17 11:31:14 +0100

    change tests so that they do not leave output files/dirs around

--- tests/lephare/test_process.py
+++ tests/lephare/test_process.py
@@ -26,7 +26,7 @@ def test_process(test_data_dir: str):
             reduced_cols.append(c)
         elif "IB679" in c:
             reduced_cols.append(c)
-    output, photozlist = lp.process(config, input[reduced_cols], write_outputs=True)
+    output, photozlist = lp.process(config, input[reduced_cols], write_outputs=False)
     # Check one of the outputs (results are terrible with just one filter and sparse z grid)
     assert np.isclose(output["Z_BEST"][0], 3.5877994546919934)
     assert len(photozlist[0].pdfmap[11].xaxis) == 51
