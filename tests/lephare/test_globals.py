33f8240dffe69a64b6ab227a8280436032d28839
Johann Cohen-Tanugi <johann.cohentanugi@gmail.com>
2025-01-09 18:37:11 +0100

    make pre-commit happy

--- tests/lephare/test_globals.py
+++ tests/lephare/test_globals.py
@@ -15,10 +15,10 @@ def test_globals_first_char():
 def test_globals_blackbody():
     assert np.isclose(blackbody(10000, 500), 1.018807e-26)
 
+
 def test_indexes_in_vec():
     v = [0.5, 0.9, 2.9, 3.5]
     val = 0.8
     assert indexes_in_vec(val, v, 0.1) == [1]
     assert indexes_in_vec(val, v, 0.01) == []
     assert indexes_in_vec(val, v, 0.3) == [0, 1]
-    
