d086e3cfe689c20e37f16830c73b6ac6952cfad0
Johann Cohen-Tanugi <johann.cohentanugi@gmail.com>
2025-01-09 18:28:39 +0100

    add unit test to indexes_in_vec

--- tests/lephare/test_globals.py
+++ tests/lephare/test_globals.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lephare._lephare import blackbody, check_first_char
+from lephare._lephare import blackbody, check_first_char, indexes_in_vec
 
 
 def test_globals_first_char():
@@ -14,3 +14,11 @@ def test_globals_first_char():
 
 def test_globals_blackbody():
     assert np.isclose(blackbody(10000, 500), 1.018807e-26)
+
+def test_indexes_in_vec():
+    v = [0.5, 0.9, 2.9, 3.5]
+    val = 0.8
+    assert indexes_in_vec(val, v, 0.1) == [1]
+    assert indexes_in_vec(val, v, 0.01) == []
+    assert indexes_in_vec(val, v, 0.3) == [0, 1]
+    
