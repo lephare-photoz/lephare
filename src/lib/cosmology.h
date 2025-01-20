3accdf199496b349dfa6c7c57381abeef2f19942
Johann Cohen-Tanugi <johann.cohentanugi@gmail.com>
2025-01-20 16:08:50 +0100

    more doxygen on cosmo class

--- src/lib/cosmology.h
+++ src/lib/cosmology.h
@@ -26,42 +26,59 @@ class cosmo {
   double _h0, _om0, _l0;
 
  public:
-  /// define a cosmology based on the triplet \f$H_0\f$, \f$\Omega_0\f$, and
-  /// \f$\Lambda_0\f$
-  cosmo(double h0 = 70,    ///< Hubble constant at present time
-        double om0 = 0.3,  ///< matter density at present time
-        double l0 = 0.7)   ///< cosmological constant
+  /// define a cosmology based on the triplet \f$H_0\f$, \f$\Omega_m\f$, and
+  /// \f$\Omega_\Lambda\f$
+  cosmo(double h0 = 70,    ///< Hubble constant at present time \f$H_0\f$
+        double om0 = 0.3,  ///< matter density at present time \f$\Omega_m\f$
+        double l0 = 0.7)   ///< cosmological constant \f$\Omega_\Lambda\f$
   {
     _h0 = h0;
     _om0 = om0;
     _l0 = l0;
   }
 
-  double distMod(double z) const;  //!< compute the distance modulus at z
+  /*!
+    compute the metric distance to z in Mpc, as
+    \f[ d_M(z) = \frac{c}{H_0}\int_0^z \frac{dz}{\sqrt{\Omega_m(1+z)^3 +
+    (1-\Omega_m-\Omega_\Lambda)(1+z)^2+\Omega_\Lambda}} \quad .\f] The
+    luminosity distance of an object at redshift \f$z\f$ is then defined as \f$
+    d_L(z)=(1+z)\,d_M(z)\f$.
+   */
   double distMet(double z) const;  //!< compute the metric distance at z
+
+  /*!
+    compute the distance modulus at z, as
+    \f[ \mu(z) = 5\log_{10}d_L(z) + 25 \quad. \f]
+   */
+  double distMod(double z) const;
+
   double time(
       double z) const;  //!< compute the cosmological time from infinity to z
+
+  //! Identity operator on a cosmo object
   inline bool operator==(const cosmo &rhs) {
     return _h0 == rhs._h0 && _om0 == rhs._om0 && _l0 == rhs._l0;
   }
+
+  //! Not equal operator on a cosmo object
   inline bool operator!=(const cosmo &rhs) {
     return _h0 != rhs._h0 || _om0 != rhs._om0 || _l0 != rhs._l0;
   }
+
+  //! Serializer of the cosmo object
   inline friend ostream &operator<<(ostream &output, const cosmo &c) {
     output << c._h0 << "," << c._om0 << "," << c._l0;
     return output;
   }
 
-  //! Return the scaling factor that brings a flux from one value to another,
-  //! based only on a change of redshift.
   /*!
     \param z Current redshift
     \param z_t Target redshift
 
     \return flux scale factor corresponding to a distance change
     from \f$z\f$ to \f$z_t\f$ :
-    \f$ scale = 10^{0.4(dm(z_t)-dm(z))}\f$
-    where \f$dm\f$ is the distance modulus for a given redshift and is obtained
+    \f[ scale = 10^{0.4\large(\mu(z_t)- \mu(z)\large)} \f]
+    where \f$\mu\f$ is the distance modulus for a given redshift and is obtained
     calling the function cosmo::distMod.
   */
   double flux_rescaling(double z, double z_t) const;
