9388d22aed86dd69046e6d7752b054c02e9bbb3b
Raphael Shirley <rshirley@mpe.mpg.de>
2025-10-14 10:42:45 +0200

    Update tests

--- tests/lephare/test_prior.py
+++ tests/lephare/test_prior.py
@@ -35,10 +35,9 @@ def test_prior(test_data_dir: str):
             weight = 1.0e-9
         return weight
 
-    output, pdfs, zgrid = lp.process(config, input[reduced_cols], prior=example_prior)
+    output, pdfs = lp.process(config, input[reduced_cols], prior=example_prior)
     # Check one of the outputs (results are terrible with just one filter and sparse z grid)
     assert ~np.isclose(output["Z_BEST"][0], 3.58577857627795)  # Should not equal the original redshift
-    assert len(zgrid) == 51
     assert np.sum(output["Z_BEST"] > 2) == 0
 
 
@@ -52,8 +51,8 @@ def test_nzprior():
     # What is the key behaviour we should be testing?
     p = lp.prior()
     s = lp.onesource(0, [0, 1])
-    s.ab = [1.0e-9, 1.0e-9]
-    s.busnorma = [1, 1]
+    # s.ab = [1.0e-9, 1.0e-9]
+    # s.busnorma = [1, 1]
     assert np.isclose(p.nz_prior(s, 0.0, 0.0, 0.0, 0.0, [0, 0]), 1)
 
 
