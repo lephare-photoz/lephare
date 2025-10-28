4d26f89989ae27b5c34311a933d3f9dca2f4a74d
Raphael Shirley <raphael.shirley@hotmail.co.uk>
2025-10-28 17:52:29 +0100

    ids were not being propagated in process
    the code was taking the index as the id

--- src/lephare/process.py
+++ src/lephare/process.py
@@ -59,7 +59,7 @@ def process(
     srclist = []
     for i in range(ng):
         one_obj = lp.onesource(i, photz.gridz)
-        one_obj.readsource(str(i), flux[i], flux_err[i], context[i], zspec[i], str(string_data[i]))
+        one_obj.readsource(str(id[i]), flux[i], flux_err[i], context[i], zspec[i], str(string_data[i]))
         photz.prep_data(one_obj)
         srclist.append(one_obj)
 
