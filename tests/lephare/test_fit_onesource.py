254411d549323d7b11afb566b71b12ba36a28dd9
OlivierIlbert <ilbert@MBP-de-Olivier.lan>
2026-01-16 08:46:18 +0100

    Add some printing

--- tests/lephare/test_fit_onesource.py
+++ tests/lephare/test_fit_onesource.py
@@ -8,10 +8,13 @@ def test_onefit():
     test_dir = os.path.abspath(os.path.dirname(__file__))
     os.environ["LEPHAREDIR"] = os.path.join(test_dir, "../data")
     os.environ["LEPHAREWORK"] = os.path.join(test_dir, "../tmp")
+    print(test_dir)
 
     # Read the config file.
     config_file = os.path.expandvars("$LEPHAREDIR/examples/COSMOS.para")
+    # config_file = os.path.join(test_data_dir, "examples/COSMOS.para")
     config = lp.read_config(config_file)
+    print(config_file)
     fltstr = "lsst/total_u.pb,lsst/total_g.pb,lsst/total_r.pb,lsst/total_i.pb,lsst/total_z.pb,lsst/total_y.pb"
     config.update(
         {
@@ -29,8 +32,8 @@ def test_onefit():
             "MOD_EXTINC": "0,100",
             "ADD_EMLINES": "0,0",
             "EM_DISPERSION": "1.",
-            "ERR_SCALE": " 0.02",
-            "ERR_FACTOR": " 1.5",
+            "ERR_SCALE": " 0.0",
+            "ERR_FACTOR": " 1.",
             "Z_INTERP": "NO",
             "MAG_ABS": "-24,-5",
             "MAG_REF": "2",
@@ -42,10 +45,12 @@ def test_onefit():
 
     # Run preparation tasks (libraries)
     lp.prepare(config)
+    print("Done reading libraries")
     assert os.path.exists(os.path.expandvars("$LEPHAREWORK/lib_mag/CE_COSMOS.doc"))
 
     # Instantiate the photoz object
     photz = lp.PhotoZ(keymap)
+    print("Done instantiate photoz")
     assert len(photz.gridz) == 21
     assert photz.gridz[0] == 0
     assert photz.gridz[20] == 1
@@ -63,6 +68,7 @@ def test_onefit():
         "test",
     )
     src.convertFlux("AB", photz.allFilters)
+    print("Done creating source")
     assert src.spec == "65"
     assert (
         np.testing.assert_almost_equal(
@@ -74,16 +80,19 @@ def test_onefit():
     photz.prep_data(src)
 
     a0 = photz.compute_offsets([])
-    assert len(a0) == 6
     src.adapt_mag(a0)
+    print("Done with offsets")
+    assert len(a0) == 6
 
     photz.fit_onesource(src)
+    print("Done with fit")
     assert np.isclose(src.zmin[0], 0.65)
     assert np.isclose(src.dmmin[0], 1.0000, atol=1e-04)
     assert np.isclose(src.imasmin[0], 1)
     assert src.chimin[0] < src.chimin[1] < src.chimin[2]
 
     photz.uncertainties_onesource(src)
+    print("Done with uncertainties")
     max_position = np.argmax(src.pdfmap[11].vPDF)
     assert max_position == 13
     assert len(src.pdfmap[11].xaxis) == 21
@@ -95,6 +104,7 @@ def test_onefit():
     assert src.zgmode[3] < src.zgmode[1] < src.zgmode[0] < src.zgmode[2] < src.zgmode[4]
 
     photz.physpara_onesource(src)
+    print("Done with physical parameters")
     assert np.isclose(src.mabs[5], 26.6285 - 42.9504 - src.kap[5], atol=3e-2)
     assert np.isclose(src.mabs[0], 30.9393 - 42.9504 - src.kap[0], atol=3e-2)
     assert np.testing.assert_almost_equal(src.absfilt, [0, 1, 2, 3, 4, 5]) is None
@@ -106,6 +116,7 @@ def test_onefit():
     fir = photz.besttemplate_onesource(src, 2, minl, maxl)
     qso = photz.besttemplate_onesource(src, 3, minl, maxl)
     star = photz.besttemplate_onesource(src, 4, minl, maxl)
+    print("Done with templates generation")
     assert len(gal2[0]) == 0
     assert len(fir[0]) == 0
     assert all(2999.0 < x < 13001.0 for x in gal1[0])
