113c71a1fd413051cb33b50a4ac0e0174f7b6f59
OlivierIlbert <ilbert@MBP-de-Olivier.lan>
2026-01-17 11:54:13 +0100

    Add tests incase of zfix, physical parameter computation with BC03

--- tests/lephare/test_fit_onesource.py
+++ tests/lephare/test_fit_onesource.py
@@ -4,7 +4,7 @@ import lephare as lp
 import numpy as np
 
 
-def test_onefit():
+def test_photoz_cosmos():
     test_dir = os.path.abspath(os.path.dirname(__file__))
     os.environ["LEPHAREDIR"] = os.path.join(test_dir, "../data")
     os.environ["LEPHAREWORK"] = os.path.join(test_dir, "../tmp")
@@ -126,3 +126,162 @@ def test_onefit():
     assert all(2999.0 < x < 13001.0 for x in star[0])
     assert all(10.0 < x < 40.0 for x in star[1])
     print("Done with the spectra")
+
+
+def test_physicalpara_bc03():
+    test_dir = os.path.abspath(os.path.dirname(__file__))
+    os.environ["LEPHAREDIR"] = os.path.join(test_dir, "../data")
+    os.environ["LEPHAREWORK"] = os.path.join(test_dir, "../tmp")
+    print(test_dir)
+
+    # Read the config file.
+    config_file = os.path.expandvars("$LEPHAREDIR/examples/COSMOS.para")
+    config = lp.read_config(config_file)
+    print(config_file)
+    lsst_files = [
+        "lsst/total_u.pb",
+        "lsst/total_g.pb",
+        "lsst/total_r.pb",
+        "lsst/total_i.pb",
+        "lsst/total_z.pb",
+        "lsst/total_y.pb",
+    ]
+    spitzer_files = ["spitzer/irac_3.pb", "spitzer/irac_4.pb"]
+    herschel_files = ["herschel/PACS_100.pb", "herschel/PACS_160.pb"]
+    fltstr = ",".join(lsst_files + spitzer_files + herschel_files)
+    config.update(
+        {
+            "VERBOSE": "NO",
+            "FILTER_LIST": fltstr,
+            "FILTER_FILE": "filters_fir",
+            "FILTER_CALIB": "0,0,0,0,0,0,1,1,1,1",
+            "ZPHOTLIB": "BC03_IR",
+            "AUTO_ADAPT": "NO",
+            "Z_STEP": "0.2,0,1",
+            "ZFIX": "YES",
+            "EB_V": "0.,0.1,0.2,0.3",
+            "MOD_EXTINC": "0,100",
+            "ADD_EMLINES": "0,0",
+            "EM_DISPERSION": "1.",
+            "ERR_SCALE": " 0.0",
+            "ERR_FACTOR": " 1.",
+            "Z_INTERP": "NO",
+            "MAG_ABS": "-24,-5",
+            "MAG_REF": "2",
+            "MABS_METHOD": "0",
+            "MABS_CONTEXT": "0",
+        }
+    )
+    keymap = lp.all_types_to_keymap(config)
+
+    # Filters
+    filterlib = lp.Filter(config_keymap=keymap)
+    filterlib.run()
+
+    # Generate a test BC03 template list
+    lines = [
+        "BC03_CHAB/bc2003_lr_m62_chab_tau1_dust00.ised_ASCII  BC03\n",
+        "BC03_CHAB/bc2003_lr_m62_chab_tau15_dust00.ised_ASCII  BC03\n",
+    ]
+    pathlist_bc03 = os.path.expandvars("$LEPHAREWORK/testBC03_MOD.list")
+    print("BC03 file ", pathlist_bc03)
+    with open(os.path.expandvars(pathlist_bc03), "w") as file:
+        file.writelines(lines)
+    # Generate a test BC03 age list
+    lines = ["0.360203\n", "2\n", "7\n"]
+    pathage_bc03 = os.path.expandvars("$LEPHAREWORK/agesBC03.txt")
+    print("BC03 ages ", pathage_bc03)
+    with open(os.path.expandvars(pathage_bc03), "w") as file:
+        file.writelines(lines)
+
+    sedlib = lp.Sedtolib(config_keymap=keymap)
+    sedlib.run(
+        typ="GAL",
+        gal_sed=pathlist_bc03,
+        gal_lib="LIB_BC03",
+        sel_age=pathage_bc03,
+    )
+
+    maglib = lp.MagGal(config_keymap=keymap)
+    maglib.run(
+        typ="GAL",
+        lib_ascii="YES",
+        gal_lib_in="LIB_BC03",
+        gal_lib_out="BC03_IR",
+        eb_v="0.,0.25,0.5",
+        mod_extinc="0,12",
+        extinc_law="SB_calzetti.dat",
+        em_lines="PHYS",
+        em_dispersion="1.",
+    )
+
+    print("Done creating libraries")
+    assert os.path.exists(os.path.expandvars("$LEPHAREWORK/lib_mag/BC03_IR.doc"))
+
+    # Instantiate the photoz object
+    photz = lp.PhotoZ(keymap)
+    print("Done instantiate photoz")
+    assert len(photz.gridz) == 6
+    assert photz.gridz[0] == 0
+    assert photz.gridz[5] == 1
+
+    # Instantiate a source (Id, gridz)
+    src = lp.onesource(101, photz.gridz)
+    # read the source, change Id, attribute mag/err, ...
+    cont = 0
+    maglib_bc03 = [51.9275, 51.2836, 50.39, 49.9945, 49.7161, 49.4793, 49.0574, 49.5759, 55.004, 56.0812]
+    # Substract 25 to get a mass of 10^10 Msol
+    maglib_bc03 = [x - 25 for x in maglib_bc03]
+    src.readsource(
+        "40",
+        maglib_bc03,
+        [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01],
+        cont,
+        0.4,
+        "ebv0.5_DM41.6845age3.60203e8",
+    )
+
+    src.convertFlux("AB", photz.allFilters)
+    print("Done creating source")
+    assert src.spec == "40"
+
+    photz.prep_data(src)
+
+    a0 = photz.compute_offsets([])
+    src.adapt_mag(a0)
+    print("Done with offsets")
+    assert len(a0) == 10
+
+    photz.fit_onesource(src)
+    print("Done with fit")
+    assert np.isclose(src.zmin[0], 0.40)
+    assert np.isclose(src.dmmin[0], 1.0000e10, atol=1e-04)
+    assert np.isclose(src.imasmin[0], 2)
+    # Low chi2 since no noise and predicted mag in input
+    assert src.chimin[0] < 1.0e-2
+
+    photz.uncertainties_onesource(src)
+    print("Done with uncertainties")
+    max_position = np.argmax(src.pdfmap[11].vPDF)
+    assert max_position == 2
+    assert len(src.pdfmap[11].xaxis) == 6
+    assert np.isclose(src.zs, 0.4, atol=1e-02)
+
+    photz.physpara_onesource(src)
+    print("Done with physical parameters")
+    assert np.isclose(src.consiz, 0.4, atol=1e-02)
+    # From the .phys
+    # mass 0.0168893 ->log10(0.0168893)+10->8.227
+    # sfr  6.50848e-11 -> log10(6.50848e-11)+10 -> -0.1865
+    # ssfr -8.4135
+    assert np.isclose(src.results["MASS_BEST"], 8.227, atol=1e-02)
+    assert np.isclose(src.results["SFR_BEST"], -0.1865, atol=1e-02)
+    assert np.isclose(src.results["SSFR_BEST"], -8.4135, atol=1e-02)
+    assert np.isclose(src.results["EBV_BEST"], 0.5, atol=1e-02)
+    assert np.isclose(src.results["AGE_BEST"], 8.55654, atol=1e-02)
+    assert np.isclose(src.results["EXTLAW_BEST"], 1, atol=1e-02)
+    assert src.massmed[1] < 8.227 < src.massmed[2]
+    assert src.agemed[1] < 8.55654 < src.agemed[2]
+    assert src.SFRmed[1] < -0.1865 < src.SFRmed[2]
+    # compare the predicted magnitudes
+    assert np.allclose(src.magm, maglib_bc03, atol=1e-3)
