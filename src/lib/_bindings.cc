1e28fdd9fe693165f321ad582c41c83df9e1058c
OlivierIlbert <ilbert@MBP-de-Olivier.lan>
2026-01-30 12:02:23 +0100

    binding nz_prior

--- src/lib/_bindings.cc
+++ src/lib/_bindings.cc
@@ -412,6 +412,7 @@ PYBIND11_MODULE(_lephare, mod) {
       .def("keepOri", &onesource::keepOri)
       .def("adapt_mag", &onesource::adapt_mag)
       .def("fit", &onesource::fit)
+      .def("nzprior", &onesource::nzprior)
       .def("mode", &onesource::mode)
       .def("rm_discrepant", &onesource::rm_discrepant)
       .def("generatePDF", &onesource::generatePDF)
