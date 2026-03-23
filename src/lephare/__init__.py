7f2fba382f519a5173fa53c09088f6179ed369db
Tototime <theotime.hallouin@gmail.com>
2026-03-23 12:18:17 +0100

    stargal_config

--- src/lephare/__init__.py
+++ src/lephare/__init__.py
@@ -23,6 +23,7 @@ from ._plot_utils import *
 from ._spec import *
 from .data_retrieval import *
 from .default_cosmos_config import *
+from .default_stargal_config import *
 from .filter import *
 from .filter_extinc import *
 from .filterSvc import *
