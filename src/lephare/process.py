a1bc98970d0130a0cfa3cd391ba8b7e24c586912
Raphael Shirley <raphael.shirley@hotmail.co.uk>
2026-04-27 15:21:50 +0200

    Refactor column name handling in process.py
    Also currently a PR to rc1.0.0 but important enough for a bug fix on main also.

--- src/lephare/process.py
+++ src/lephare/process.py
@@ -192,15 +192,20 @@ def table_to_data(config, input, col_names=None, standard_names=False):
         col_names += ["zspec"]
         col_names += ["string_input"]
     else:
-        print("Using user columns from input table assuming they are in the standard order.")
-        assert len(input.colnames) == 2 * n_filters + 4
+        cat_fmt = config.get("CAT_FMT", lp.keyword("CAT_FMT", "MEME"))
+        print(f"Using user columns from input table assuming they are in the standard {cat_fmt.value} order.")
+        assert len(input_table.colnames) == 2 * n_filters + 4
         col_names = list(np.full(2 * n_filters + 4, ""))
-        col_names[0] = input.colnames[0]
-        col_names[1 : 2 * n_filters : 2] = input.colnames[1 : 2 * n_filters : 2]
-        col_names[2 : 2 * n_filters + 1 : 2] = input.colnames[2 : 2 * n_filters + 1 : 2]
-        col_names[-3] = input.colnames[-3]
-        col_names[-2] = input.colnames[-2]
-        col_names[-1] = input.colnames[-1]
+        col_names[0] = input_table.colnames[0]
+        if cat_fmt.value == "MEME":
+            col_names[1 : 2 * n_filters : 2] = input_table.colnames[1 : 2 * n_filters : 2]
+            col_names[2 : 2 * n_filters + 1 : 2] = input_table.colnames[2 : 2 * n_filters + 1 : 2]
+        elif cat_fmt.value == "MMEE":
+            col_names[1 : 2 * n_filters : 2] = input_table.colnames[1 : n_filters + 1]
+            col_names[2 : 2 * n_filters + 1 : 2] = input_table.colnames[n_filters + 1 : 2 * n_filters + 1]
+        col_names[-3] = input_table.colnames[-3]
+        col_names[-2] = input_table.colnames[-2]
+        col_names[-1] = input_table.colnames[-1]
         # print(col_names)
 
     assert len(col_names) == 2 * n_filters + 4
