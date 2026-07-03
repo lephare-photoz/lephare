94e5ac32d737e26e9d16ebe8885da8840c213542
OlivierIlbert <ilbert@eduroam-1-74-4.univ-amu.fr>
2026-07-03 12:02:44 +0200

    bug identified by sentry on Cardelli

--- src/lib/mag.cpp
+++ src/lib/mag.cpp
@@ -96,7 +96,7 @@ Mag::Mag(keymap& key_analysed)
 
     // computes the galactic extinction
     double dlbd = (lmax - lmin) / 400.;
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 1; i < 10000; i++) {
       lextg = lmin + double(i - 1) * dlbd;
       extg = cardelli_law(lextg);
       milkyWayExtinction.add_element(lextg, extg);
