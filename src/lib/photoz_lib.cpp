6f08f224cbf92bf207c849995e6b726fd4fce769
OlivierIlbert <olivier.ilbert@lam.fr>
2026-09-04 14:50:02 +0200

    Improve error message when MW EBV correction can not be applied

--- src/lib/photoz_lib.cpp
+++ src/lib/photoz_lib.cpp
@@ -1758,7 +1758,7 @@ void PhotoZ::read_mw_ebv(vector<onesource*> sources) {
         throw std::runtime_error(
             "External MW_EBV_FILE not found: " + mw_ebv_file +
             " and global EBV not defined." +
-            "Can not apply MW dust correction" + "as required.");
+            "Can not apply MW dust correction as required.");
       }
 
       // Build a lookup from source ID -> source pointer
@@ -1795,7 +1795,12 @@ void PhotoZ::read_mw_ebv(vector<onesource*> sources) {
             " applied to all sources while the option is used.");
       }
     }
+  } else {
+    throw std::runtime_error("External MW_EBV_FILE not found: " + mw_ebv_file +
+                             " and global EBV not defined." +
+                             "Can not apply MW dust correction as required.");
   }
+
   return;
 }
 
